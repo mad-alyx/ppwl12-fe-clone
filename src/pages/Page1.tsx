2c079f9e1364d457bdf28bc5f2744bd10064233c
mad-alyx <h1101241064@student.untan.ac.id>
2026-04-29 22:23:41 +0700

    ale content

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,11 +1,17 @@
 import TiketNavbar from '../components/TiketNavbar';
 import TiketHero from '../components/TiketHero';
+import AleContent from '../components/alecontent'; // 1. Import komponen kamu
 
 export default function Page1() {
   return (
-    <div>
+    <div className="bg-[#f4f7fa] min-h-screen"> 
       <TiketNavbar />
       <TiketHero />
+      
+      {/* 2. Tambahkan komponen kamu di sini */}
+      <AleContent /> 
+      
+      {/* Nanti kerjaan Iren & Nayla tinggal tumpuk di bawah sini */}
     </div>
   );
 }
