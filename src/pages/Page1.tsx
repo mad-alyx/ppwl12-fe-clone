fc7ec1a6441e13a97eb1f79618969030ca7bbf85
Irenensy <h1101241031@student.untan.ac.id>
2026-04-29 21:51:46 +0700

    Bus Travel + Promo Section

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,9 +1,13 @@
 import HeroSearchForm from "../components/HeroSearchForm";
+import BusTravelSection from "@/components/BusTravelSection";
+import PromoSections from "@/components/PromoSections";
 
 export default function Page1() {
   return (
     <main className="w-full">
       <HeroSearchForm />
+      <BusTravelSection />
+      <PromoSections />
     </main>
   );
 }
