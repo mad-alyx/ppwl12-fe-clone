e9c33e63967a07e7fd710e97924ed54747e9559d
ctrl-h1101241002 <h1101241002@student.untan.ac.id>
2026-04-29 22:50:58 +0700

    perbaikan popup

--- src/components/PromoPopup.tsx
+++ src/components/PromoPopup.tsx
@@ -2,33 +2,37 @@ import { useState } from 'react';
 import appBannerPromoImg from '/img/gambar-popup1.png'; 
 
 const PromoPopup = () => {
-  // State untuk kontrol buka/tutup popup
   const [isOpen, setIsOpen] = useState(true);
-
-  // Jika closed, jangan tampilkan apa-apa
   if (!isOpen) return null;
-
   return (
-    // Container Overlay (Penuhi seluruh layar, z-index tinggi agar di depan)
-    <div className="fixed inset-0 bg-black/70 z-9999 flex items-center justify-center p-4">
+    <div className="fixed inset-0 bg-black/35 z-9999 flex items-center justify-center p-4">
       
-      {/* Box Popup (Batas lebar maksimal) */}
-      <div className="relative w-full max-w-90 animate-in fade-in zoom-in duration-300">
+      {/* Box Popup  */}
+      <div className="relative w-full max-w-80 animate-in fade-in zoom-in duration-300">
         
-        {/* Tombol Close (X) Putih di pojok kanan atas */}
+        {/* Tombol Close (X)*/}
         <button 
           onClick={() => setIsOpen(false)}
-          className="absolute -top-10 right-0 text-white hover:text-gray-300 transition-colors"
+          className="absolute -top-10 right-2 w-8 h-8 bg-white rounded-full flex items-center justify-center"
         >
           {/* Icon X (SVG) */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <svg 
+            width="20" 
+            height="20" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="#4d4f56" 
+            strokeWidth="2.5" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+        >
             <line x1="18" y1="6" x2="6" y2="18"></line>
             <line x1="6" y1="6" x2="18" y2="18"></line>
-          </svg>
+        </svg>
         </button>
 
         {/* Konten Utama */}
-        <div className="overflow-hidden shadow-2xl">
+        <div className="container mx-auto px-4 max-w-310">
           
           {/* BAGIAN GAMBAR POPUP*/}
           <div className="relative">
@@ -39,11 +43,11 @@ const PromoPopup = () => {
             />
           </div>
 
-          {/* Bagian Tombol Biru di bawah gambar */}
+          {/* Button biru */}
           <div className="p-5 bg-white">
             <button 
               onClick={() => setIsOpen(false)}
-              className="w-full bg-[#007CFF] hover:bg-[#5BAAFF] text-white font-tiket font-bold py-3 px-6 rounded-full transition-colors text-sm"
+              className="w-auto bg-[#007CFF] hover:bg-[#5BAAFF] text-white font-tiket font-bold py-3 px-6 rounded-md transition-colors text-md"
             >
               Klaim penerbangan murah
             </button>
