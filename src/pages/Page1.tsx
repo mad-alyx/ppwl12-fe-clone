6fcc40132609354c0ce56f3d8c539e7ef461e924
mad-alyx <h1101241064@student.untan.ac.id>
2026-04-30 07:10:59 +0700

    fix: gabungkan layout page 2 dan fahdil -ale

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,7 +1,21 @@
-export default function Page1() {
-    return (
-        <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
-            <h1>Page 1</h1>
-        </div>
-    )
-}
+import Navbar from '../components/Navbar';
+import TiketNavbar from '../components/TiketNavbar';
+import TiketHero from '@/components/TiketHero';
+import HeroSearchForm from '@/components/HeroSearchForm';
+import AleContent from '../components/alecontent';
+import PromoBanners from '@/components/PromoBanners';
+import DomesticDeals from '@/components/DomesticDeals';
+import LastBanner from '@/components/LastBanner';
+import PromoFlight from '@/components/PromoFlight';
+
+const Page1 = () => {
+  return (
+    <div className="min-h-screen bg-[#F5F7FA]"> 
+      <TiketNavbar /> {/* Navbar khusus untuk halaman ini */}
+      <TiketHero /> {/* Hero section dengan gambar dan teks */}
+      <AleContent /> {/* Konten utama yang kamu buat */}
+    </div>
+  );
+};
+
+export default Page1; 
--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,3 +1,13 @@
+import TiketNavbar from '../components/TiketNavbar';
+import DomesticDeals from '../components/DomesticDeals';
+import FlightDeals from '../components/FlightDeals';
+import PromoBanners from '../components/PromoBanners';
+import PromoFlight from '../components/PromoFlight';
+import RecentSearch from '../components/RecentSearch';
+import LastBanner from '../components/LastBanner';
+import FlightSearch from '../components/FlightSearchCard';
+
+// Import komponen dari Fahdil (Upstream)
 import { ImageBanner } from "../components/shared/imageBanner";
 import { CheckInGrid } from "../components/checkIn/checkInGrid";
 import { TabbedLinkGroup } from "../components/seoAndNav/tabbedLinkGroup";
@@ -6,53 +16,69 @@ import { SeoSections } from "../components/seoAndNav/seoSections";
 
 export default function Page2() {
   return (
-    // Hapus warna background dari tag main agar tidak bertabrakan
-    <main className="w-full min-h-screen font-sans">
+    <div className="min-h-screen bg-[#F5F7FA]">
+      <TiketNavbar />
 
-      <div className="w-full bg-[rgb(237,240,247)] pt-8 pb-1">
-        <div className="max-w-[1080px] mx-auto px-4 md:px-0">
-          <ImageBanner 
-            imgSrc="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/mobile-modules/2025/05/31/462d81ae-7be2-4612-adac-accf3c6858dc-1748680446310-29096c2c8631e762d29d4dee7ab2d005.jpg"
-            altText="Promo PayLater Tiket Pesawat"
-            linkUrl="https://www.tiket.com/id-id/promo/pesawat/pesawat-paylater"
-          />
-
-          <div className="my-25">
-            <CheckInGrid />
-          </div>
+      <main className="w-full font-sans">
+        {/* --- BAGIAN KAMU (Ditaruh di Atas) --- */}
+        <div className="max-w-[1200px] mx-auto px-4 py-10 space-y-16">
+          <FlightSearch />
+          <RecentSearch />
           
-          <ImageBanner 
-            imgSrc="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/mobile-modules/2023/08/24/5e19cd0d-2b7d-4829-a3b6-187e5ede369b-1692881488728-cc647383bc6803b96baf78ad44112d2e.jpg"
-            altText="Download Aplikasi Tiket.com"
-            linkUrl="https://tiket.onelink.me/3719787275/7jadpyqb"
-            aspectRatioClass="aspect-[4/1]"
-          />
-
-          <div className="mt-8">
-            <TabbedLinkGroup />
+          <div className="grid gap-16">
+            <PromoBanners />
+            <FlightDeals />
+            <PromoFlight />
+            <DomesticDeals />
           </div>
-
-          <ImageBanner 
-            imgSrc="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/mobile-modules/2025/02/10/7f4c581f-448e-42d4-b68a-c45eed6070e7-1739173752122-50f5b7d2035b6dadd6d25429e6798173.png"
-            altText="Cek Daftar Hari Libur Nasional"
-            linkUrl="https://www.tiket.com/id-id/explore/libur-nasional-dan-cuti-bersama-2026"
-            aspectRatioClass="aspect-[4/1]"
-          />
+          <LastBanner />
         </div>
-      </div>
 
-      <div className="w-full bg-[rgb(255,255,255)] pt-10 pb-2">
-        <div className="max-w-[1080px] mx-auto px-4 md:px-0">
-          <SeoSections />
+        {/* --- BAGIAN FAHDIL (Gabungan SEO & Konten Tambahan) --- */}
+        <div className="w-full bg-[rgb(237,240,247)] pt-8 pb-1">
+          <div className="max-w-[1080px] mx-auto px-4 md:px-0">
+            <ImageBanner 
+              imgSrc="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/mobile-modules/2025/05/31/462d81ae-7be2-4612-adac-accf3c6858dc-1748680446310-29096c2c8631e762d29d4dee7ab2d005.jpg"
+              altText="Promo PayLater Tiket Pesawat"
+              linkUrl="https://www.tiket.com/id-id/promo/pesawat/pesawat-paylater"
+            />
+
+            <div className="my-10"> {/* Disesuaikan agar tidak terlalu renggang */}
+              <CheckInGrid />
+            </div>
+            
+            <ImageBanner 
+              imgSrc="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/mobile-modules/2023/08/24/5e19cd0d-2b7d-4829-a3b6-187e5ede369b-1692881488728-cc647383bc6803b96baf78ad44112d2e.jpg"
+              altText="Download Aplikasi Tiket.com"
+              linkUrl="https://tiket.onelink.me/3719787275/7jadpyqb"
+              aspectRatioClass="aspect-[4/1]"
+            />
+
+            <div className="mt-8">
+              <TabbedLinkGroup />
+            </div>
+
+            <ImageBanner 
+              imgSrc="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/mobile-modules/2025/02/10/7f4c581f-448e-42d4-b68a-c45eed6070e7-1739173752122-50f5b7d2035b6dadd6d25429e6798173.png"
+              altText="Cek Daftar Hari Libur Nasional"
+              linkUrl="https://www.tiket.com/id-id/explore/libur-nasional-dan-cuti-bersama-2026"
+              aspectRatioClass="aspect-[4/1]"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="w-full bg-[rgb(237,240,247)] pt-10 pb-2">
-        <div className="max-w-[1080px] mx-auto px-4 md:px-0">
-          <PromoRegionTabs />
+        <div className="w-full bg-[rgb(255,255,255)] pt-10 pb-2">
+          <div className="max-w-[1080px] mx-auto px-4 md:px-0">
+            <SeoSections />
+          </div>
         </div>
-      </div>
 
-    </main>
+        <div className="w-full bg-[rgb(237,240,247)] pt-10 pb-2">
+          <div className="max-w-[1080px] mx-auto px-4 md:px-0">
+            <PromoRegionTabs />
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
